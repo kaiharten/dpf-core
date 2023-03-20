3fe75832290e1e4f0894b9f96a232ec837ca330b
Kai Edorado Marine <67263873+kai-edorado@users.noreply.github.com>
2023-03-20 17:56:41 +0000

    Changed example to add more plugins to a package

--- examples/create_and_load_plugin/create.cpp
+++ examples/create_and_load_plugin/create.cpp
@@ -5,18 +5,41 @@
 #include "dpf-core/core.h"
 #include "interface.h"
 
-class ExampleA : public ExampleType {
+class Bob : public ExampleType {
  public:
-  ExampleA(void) {}
-  std::string name() override { return "ExampleA"; }
+  Bob(void) {}
+  std::string name() override { return "Bob"; }
   int DoSomething(void) override { return 15; }
 };
 
-class ExampleACreator : public ExampleTypeCreator {
+class BobCreator : public ExampleTypeCreator {
  public:
-  ExampleACreator(void)
-      : ExampleTypeCreator("ExampleAPlugin", ExampleA::version) {}
-  ExampleA* create() override { return new ExampleA(); }
+  BobCreator(void) : ExampleTypeCreator("BobPlugin", Bob::version) {}
+  Bob* create() override { return new Bob(); }
+};
+
+class Person : public ExampleType {
+ public:
+  explicit Person(std::string name, int age) : _name(name), _age(age) {}
+  std::string name() override { return _name; }
+  int DoSomething(void) override { return _age; }
+
+ private:
+  std::string _name;
+  int _age;
+};
+
+class PersonCreator : public ExampleTypeCreator {
+ public:
+  explicit PersonCreator(std::string name, int age)
+      : _name(name),
+        _age(age),
+        ExampleTypeCreator(name + "Plugin", Person::version) {}
+  Person* create() override { return new Person(_name, _age); }
+
+ private:
+  std::string _name;
+  int _age;
 };
 
 #ifdef _WIN32
@@ -26,5 +49,9 @@ class ExampleACreator : public ExampleTypeCreator {
 #endif
 
 extern "C" EXPORTIT void register_dpf_package(dpf::Core* core) {
-  core->AddPlugin(new ExampleACreator());
+  core->AddPlugin(new BobCreator());
+  core->AddPlugin(new PersonCreator("Alice", 56));
+  core->AddPlugin(new PersonCreator("William", 30));
+  core->AddPlugin(new PersonCreator("Billiam", 30));
 }
+
--- examples/create_and_load_plugin/load.cpp
+++ examples/create_and_load_plugin/load.cpp
@@ -5,11 +5,10 @@
 #include "dpf-core/core.h"
 #include "interface.h"
 
-
 int main(void) {
   dpf::Core core;
   core.AddServer(ExampleType::ServerName(), ExampleType::version);
-  
+
   bool status = core.LoadLibrary(
       "/workspaces/dpf-core/build/examples/create_and_load_plugin/"
       "libplugin.so");
@@ -18,7 +17,8 @@ int main(void) {
 
   std::vector<ExampleType*> plugins;
   std::string server_name = ExampleType::ServerName();
-  std::vector<ExampleTypeCreator*> examples = core.GetAllPlugins<ExampleTypeCreator>(server_name);
+  std::vector<ExampleTypeCreator*> examples =
+      core.GetAllPlugins<ExampleTypeCreator>(server_name);
 
   if (examples.size() > 0) {
     for (std::vector<ExampleTypeCreator*>::iterator iter = examples.begin();
@@ -26,7 +26,8 @@ int main(void) {
       ExampleTypeCreator& plugin = *(*iter);
       std::cout << plugin.ServerName() << std::endl;
       ExampleType* instance = plugin.create();
-      std::cout << instance->DoSomething() << std::endl;
+      std::cout << instance->name() << " is " << instance->DoSomething()
+                << " years old." << std::endl;
     }
   }
   return 0;
